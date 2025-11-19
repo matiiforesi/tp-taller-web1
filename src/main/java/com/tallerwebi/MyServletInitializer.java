3650683618db11e456e23acf40ae72bd76aa5114
juan <garciapjuanma@gmail.com>
2025-11-19 18:59:52 -0300

    Mapear respuesta api

--- src/main/java/com/tallerwebi/MyServletInitializer.java
+++ src/main/java/com/tallerwebi/MyServletInitializer.java
@@ -3,7 +3,10 @@ package com.tallerwebi;
 import com.tallerwebi.config.DatabaseInitializationConfig;
 import com.tallerwebi.config.HibernateConfig;
 import com.tallerwebi.config.SpringWebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class MyServletInitializer
         extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -23,5 +26,13 @@ public class MyServletInitializer
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{characterEncodingFilter};
     }
 }
